0ad7bf63dfeaa8558385203769b38fdc603d6be3
Gavin Lee <lee.gavin.k@gmail.com>
2021-07-20 22:56:18 +0200

    adjust legend when all cancers present

--- R/visPed.R
+++ R/visPed.R
@@ -65,7 +65,12 @@ visPed <- function(ped, annot.cancers = "all", annot.feature = "CurAge",
     cancer_status <- ped[, paste0("isAff", cancers_in_pedigree), drop = FALSE]
     non_appearing_cancers <- paste0("isAff", setdiff(supported_cancers,
                                                      cancers_in_pedigree))
-    cancer_status[non_appearing_cancers] <- 0
+    if (non_appearing_cancers == "isAff") {
+      # if all cancers present, don't need to do anything
+    } else {
+      # when there are some missing cancers, set them all to 0
+      cancer_status[non_appearing_cancers] <- 0
+    }
     cancers_to_plot <- cancers_in_pedigree
   }
 
